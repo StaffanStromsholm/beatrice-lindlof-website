fd715a707011ce5bb164b5b7fce2631b4225bcb9
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2022-03-11 22:14:33 +0200

    added animation to hero banner

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import paintRollerSvg from "../images/roller-paint-roller-svgrepo-com.svg";
 import { E_Font } from "./Font";
-import { keyframes } from 'styled-components'
+import { keyframes } from "styled-components";
 
 const swipe = keyframes`
  0% { left: -1000px; }
- 100% { left: 2000px; }
-`
+ 100% { left: 1500px; }
+`;
+
+const fadein = keyframes`
+ 0% { opacity: 0; }
+ 70% { opacity: 0; }
+ 100% { opacity: 1; }
+`;
 
 const HeroBannerWrapper = styled.div`
     width: 100%;
@@ -26,23 +32,27 @@ const LogoWrapper = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    animation-name: ${fadein};
+    animation-duration: 1.5s;
 `;
 
 const NickName = styled.div`
-font-family: 'Sacramento', cursive;
-font-size: 3rem;
-color: white;
-margin-left: -10px;
+    font-family: "Sacramento", cursive;
+    font-size: 3rem;
+    color: white;
+    margin-left: -10px;
 `;
 
 const PaintRoller = styled.img`
     width: 30px;
-    filter: invert(100%) sepia(0%) saturate(1352%) hue-rotate(87deg) brightness(119%) contrast(119%);
+    filter: invert(100%) sepia(0%) saturate(1352%) hue-rotate(87deg)
+        brightness(119%) contrast(119%);
 `;
 
 const PaintRollerSwipe = styled.img`
     width: 400px;
-    filter: invert(100%) sepia(0%) saturate(1352%) hue-rotate(87deg) brightness(119%) contrast(119%);
+    filter: invert(100%) sepia(0%) saturate(1352%) hue-rotate(87deg)
+        brightness(119%) contrast(119%);
     transform: rotate(-90deg);
     position: absolute;
     left: -1000px;
@@ -59,7 +69,9 @@ color: white;
 
 const CatchPhrase = styled.div`
 color: #776622;
-font-family: ${E_Font.FONT_FAMILY}`;
+font-family: ${E_Font.FONT_FAMILY}
+animation-name: ${fadein};
+    animation-duration: 1.5s;`;
 
 export default function HeroBanner() {
     return (
